4a3a6a9b4fca664d679c0f58dc21f86128a5179e
Ryker Dial <ryker.dial@gmail.com>
2020-01-12 22:55:18 -0600

    Basic structure of project, uses ethernet of STM32 to send ros messages to Jetson Nano.

---
+++ firmware/margaret_stm32f767zi_fw/src/main.cpp
@@ -0,0 +1,28 @@
+#define STM32F767ZI // Define so rosserial switches to using ethernet
+
+#include <Arduino.h>
+
+#include <ros.h>
+#include <std_msgs/String.h>
+
+ros::NodeHandle  nh;
+
+std_msgs::String str_msg;
+ros::Publisher chatter("chatter", &str_msg);
+
+char hello[13] = "hello world!";
+
+void setup()
+{
+  Serial.begin(115200);
+  nh.initNode();
+  nh.advertise(chatter);
+}
+
+void loop()
+{
+  str_msg.data = hello;
+  chatter.publish( &str_msg );
+  nh.spinOnce();
+  delay(1000);
+}
